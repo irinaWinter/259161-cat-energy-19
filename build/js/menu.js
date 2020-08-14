459a13cb66e30f7a4ba532b11e9d835b6bed7010
Itina Winter <lobanova.irina.96@mail.ru>
2020-08-14 13:25:12 +0300

    Добавляет build

---
+++ build/js/menu.js
@@ -0,0 +1,1 @@
+var hamburger=document.querySelector(".hamburger"),menu=document.querySelector(".main-nav");hamburger.classList.remove("hamburger--nojs"),menu.classList.remove("main-nav--nojs"),hamburger.addEventListener("click",function(){hamburger.classList.toggle("hamburger--close"),menu.classList.toggle("main-nav--opened")});
